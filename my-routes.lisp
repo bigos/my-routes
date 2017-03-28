4c54446a69e3c872c12dcb1bc50cb12a21012c79
Jacek Podkanski <ruby.object@googlemail.com>
2017-03-28 02:02:15 +0100

    created custom dispatcher

--- my-routes.lisp
+++ my-routes.lisp
@@ -1,10 +1,22 @@
 (in-package #:my-routes)
 
+;;; move to server
+(defun create-custom-dispatcher (prefix handler)
+  "Creates a request dispatch function which will dispatch to the
+function denoted by HANDLER if the file name of the current request
+starts with the string PREFIX."
+  (lambda (request)
+    (let ((mismatch (mismatch (hunchentoot::script-name request) prefix
+                              :test #'char=)))
+      (and (or (null mismatch)
+               (>= mismatch (length prefix)))
+           handler))))
+
 ;;; list of functions for adding to the dispatch-table
 (server:add-routes
- (list (hunchentoot:create-prefix-dispatcher "/foo" 'handlers::foo1)
-       (hunchentoot:create-prefix-dispatcher "/baz" 'handlers::baz)
-       (hunchentoot:create-prefix-dispatcher "/bar" 'handlers::bar)))
+ (list (create-custom-dispatcher "/foo" 'handlers::foo1)
+       (create-custom-dispatcher "/baz" 'handlers::baz)
+       (create-custom-dispatcher "/bar" 'handlers::bar)))
 
 ;;; Start VHOST
 (server:restart-acceptor)
