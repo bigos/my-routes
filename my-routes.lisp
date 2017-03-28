f8daf8a95d890b111910e75f13a71b8635814ed0
Jacek Podkanski <ruby.object@googlemail.com>
2017-03-28 09:03:28 +0100

    trying

--- my-routes.lisp
+++ my-routes.lisp
@@ -1,5 +1,24 @@
 (in-package #:my-routes)
 
+(defun split-by-slash (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\/ string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun build-regex (str)
+  (let ((chunks (cdr (split-by-slash str))))
+    (with-output-to-string (s)
+      (loop for p in (mapcar
+                      (lambda (c) (if (equal (subseq c 0 1) ":") "\w*" c))
+                      chunks)
+         do (format s "~a/~A" "\\"  p)))))
+
+;;; regexp can be converted to parse tree using
+;;; (cl-ppcre:parse-string "/ala/.*/kota" )
+;;; parse tree can replace regexp
+(cl-ppcre:all-matches-as-strings '(:SEQUENCE "/ala/" (:GREEDY-REPETITION 0 NIL :EVERYTHING) "/kota")  "/ala/ma/kota")
+
 ;;; list of functions for adding to the dispatch-table
 (server:add-routes
  (list (server:create-custom-dispatcher "/foo" 'handlers::foo1)
