2083192967ab8001de9a20e1388e8432d2b4c90b
Jacek Podkanski <ruby.object@googlemail.com>
2017-03-28 02:45:26 +0100

    thinking

--- server.lisp
+++ server.lisp
@@ -20,18 +20,18 @@
   (:default-initargs                   ; default-initargs must be used
    :address "127.0.0.1"))              ; because ACCEPTOR uses it
 
+;;; Instantiate VHOST
+(defvar vhost1 (make-instance 'vhost :port 5000))
+
 ;;; Specialise ACCEPTOR-DISPATCH-REQUEST for VHOSTs
 (defmethod acceptor-dispatch-request ((vhost vhost) request)
   ;; try REQUEST on each dispatcher in turn
-  (mapc (lambda (dispatcher)
+  (mapc (lambda (dispatcher) ; as defined in the function create-custom-dispatcher
           (let ((handler (funcall dispatcher request)))
             (when handler ; Handler found. FUNCALL it and return result
               (return-from tbnl:acceptor-dispatch-request (funcall handler)))))
         (dispatch-table vhost))
   (call-next-method))
-
-;;; Instantiate VHOST
-(defvar vhost1 (make-instance 'vhost :port 5000))
 
 (defun create-custom-dispatcher (prefix handler)
   "Creates a request dispatch function which will dispatch to the
